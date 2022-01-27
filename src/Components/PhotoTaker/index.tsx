dce24e5c0b7780f3da6ea9d1b14f8636b2b01a3e
Marian Condorachi <marian.condorachi@gmail.com>
2022-01-27 19:18:11 +0200

    added excel and scanner

--- src/Components/PhotoTaker/index.tsx
+++ src/Components/PhotoTaker/index.tsx
@@ -1,3 +1,23 @@
+import { useState } from "react";
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
+
 export const PhotoTaker = () => {
-  return <div className="photo-taker">photo-taker</div>;
+  const [data, setData] = useState("Not Found");
+
+  return (
+    <div className="photo-taker">
+      <>
+        <BarcodeScannerComponent
+          width={500}
+          height={500}
+          onUpdate={(err, result) => {
+            // @ts-expect-error etcetc
+            if (result) setData(result?.text);
+            else setData("Not Found");
+          }}
+        />
+        <p>{data}</p>
+      </>
+    </div>
+  );
 };
