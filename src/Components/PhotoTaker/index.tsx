12283bd56ff192b87bb09f10b2730234e2d0293a
Marian Condorachi <marian.condorachi@gmail.com>
2022-01-28 18:59:08 +0200

    test

--- src/Components/PhotoTaker/index.tsx
+++ src/Components/PhotoTaker/index.tsx
@@ -1,13 +1,32 @@
-import { useState } from "react";
-import BarcodeScannerComponent from "react-qr-barcode-scanner";
+import { useEffect, useState } from "react";
+import BarCodeScanner from "barcode-react-scanner";
 
 export const PhotoTaker = () => {
-  const [data, setData] = useState("Not Found");
+  const [data1, setData1] = useState("Not Found");
+  const [data2, setData2] = useState("Not Found");
+
+  const handleScan = (data: any) => {
+    setData1(data);
+  };
+
+  const handleClick = async () => {
+    // scanner = await BarcodeScanner.createInstance();
+    // scanner.onFrameRead = (results: TextResult[]) => {
+    //   if (results.length > 0) console.log(results);
+    // };
+    // scanner.onUnduplicatedRead = (txt: string, result: TextResult) => {
+    //   alert(txt);
+    // };
+    // await scanner.show();
+  };
 
   return (
     <div className="photo-taker">
+      <button id="btn-show-scanner" onClick={handleClick}>
+        Show Barcode Scanner
+      </button>
       <>
-        <BarcodeScannerComponent
+        {/* <BarcodeScannerComponent
           width={500}
           height={500}
           onUpdate={(err, result) => {
@@ -15,8 +34,16 @@ export const PhotoTaker = () => {
             if (result) setData(result?.text);
             else setData("Not Found");
           }}
+        /> */}
+        <BarCodeScanner
+          onUpdate={(err, resp): void => {
+            if (resp) {
+              setData1(resp.getText());
+            }
+          }}
         />
-        <p>{data}</p>
+        <p>{data1}</p>
+        <p>{data2}</p>
       </>
     </div>
   );
